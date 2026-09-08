ecf2bf7cb2d13d5504cd7cddc61d27e38600b53d
rchtsang <rtsang1@stevens.edu>
2026-09-08 00:10:43 -0400

    gpt5.6-sol expand abi headers with module records, mmu masks, fault codes, and constants

--- kernel/include/minemu/boot.h
+++ kernel/include/minemu/boot.h
@@ -6,6 +6,9 @@
 #define MINEMU_BOOT_INFO_MAGIC UINT32_C(0x4d424f4f)
 #define MINEMU_ABI_VERSION UINT16_C(1)
 #define MINEMU_BOOT_INFO_VADDR UINT32_C(0xc0007000)
+#define MINEMU_SEGMENT_READABLE UINT32_C(1)
+#define MINEMU_SEGMENT_WRITABLE UINT32_C(2)
+#define MINEMU_SEGMENT_EXECUTABLE UINT32_C(4)
 
 struct minemu_boot_info {
     uint32_t magic;
@@ -22,6 +25,27 @@ struct minemu_boot_info {
     uint32_t reserved[6];
 };
 
+struct minemu_module_record {
+    uint32_t name_offset;
+    uint32_t name_length;
+    uint32_t segment_table_offset;
+    uint32_t segment_count;
+    uint32_t entry_vaddr;
+    uint32_t flags;
+    uint32_t reserved[2];
+};
+
+struct minemu_module_segment {
+    uint32_t data_offset;
+    uint32_t virtual_address;
+    uint32_t file_size;
+    uint32_t memory_size;
+    uint32_t flags;
+    uint32_t reserved[3];
+};
+
 _Static_assert(sizeof(struct minemu_boot_info) == 64, "boot-info size");
+_Static_assert(sizeof(struct minemu_module_record) == 32, "module-record size");
+_Static_assert(sizeof(struct minemu_module_segment) == 32, "module-segment size");
 
 #endif
